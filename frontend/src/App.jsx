d08956a4269a851242777aae3da3dfdd7c12f7f3
dgsw18 <hhoc10661@gmail.com>
2026-08-12 18:03:49 +0900

    다시 복구

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import { Routes, Route, Link, Form } from 'react-router-dom';
-import './App.css'
+//라우터 부
+import './css/App.css'
+import Home from './pages/Home.jsx';
 
+// 끝부분
 function App() {
   return (
     <>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </>
-  )
+  );
 }
-export default App
+
+
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/Home.css'
+
+
+function Home() {
+  return (
+    <div className='home'>
+        <p>테스트용</p>
+    </div>
+  );
+}
+
+export default Home;
