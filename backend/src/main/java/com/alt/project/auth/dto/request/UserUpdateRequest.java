5f47228a4108936da871d6908000da23b030e7b6
dgsw51 <lkj.100912@gmail.com>
2026-08-26 15:48:09 +0900

    Refactor: 어노테이션 수정

--- backend/src/main/java/com/alt/project/auth/dto/request/UserUpdateRequest.java
+++ backend/src/main/java/com/alt/project/auth/dto/request/UserUpdateRequest.java
@@ -1,7 +1,5 @@
 package com.alt.project.auth.dto.request;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
